cb641b891ee5427192189a4efa08d3554ab6f2c4
Jake <jake@hwll.me>
2023-04-02 13:01:09 +1000

    feat: add numbers and sounds

--- src/grid.ts
+++ src/grid.ts
@@ -1,7 +1,8 @@
-export const Button = ({ dataId, bomb }: { dataId: string, bomb: boolean }) => {
+export const Button = ({ dataId, bomb, surrounds }: { dataId: string, bomb: boolean, surrounds: number }) => {
   const el = document.createElement("button");
   el.className = [""].join(" ");
   el.setAttribute("data-id", dataId);
+  el.setAttribute("data-surround", `${surrounds}`);
 
   if ( bomb ) {
     el.className = [
@@ -11,55 +12,103 @@ export const Button = ({ dataId, bomb }: { dataId: string, bomb: boolean }) => {
   }
 
   el.addEventListener("click", () => {
-    el.className = [
-      "active",
-      ...el.className.split(" ")
-    ].join(' ');
+    if (el.classList.contains("active")) return;
+    el.className = ["active", ...el.className.split(" ")].join(" ");
+
+    if ( bomb ) {
+      var loseSound = new Audio("/audio/lose.wav");
+      loseSound.play();
+      document.querySelectorAll('.bomb').forEach((el) => {
+        el.className = ["active", ...el.className.split(" ")].join(" ");
+      })
+    } else {
+      var tickSound = new Audio("/audio/tick.wav");
+      tickSound.play()
+    }
   });
 
   return el;
 };
 
 export const Row = (children: HTMLElement[], id: string) => {
-    const el = document.createElement("div")
-    el.id = `row-${id}`
-    children.map((v) => el.appendChild(v))
-    return el
+  const el = document.createElement("div")
+  el.id = `row-${id}`
+  children.map((v) => el.appendChild(v))
+  return el
 }
 
 export const generateGrid = (width: number, height: number) => {
-    const size = width * height
-    const field = Array(size).fill(-1);
-    const bombs = Math.round((10 / size) * size);
+  const size = width * height
+  const field = Array(size).fill(-1);
+  const bombs = Math.round((10 / size) * size);
 
-    // Add bombs!
-    const validBombLocations = [...Array(size).keys()];
-    for (var i = 0; i < bombs; i++) {
-      // Get a random location.
-      var pos = Math.round(Math.random() * (validBombLocations.length - 0) + 0);
-      // Make it a bomb.
-      field[pos] = 9
-      // Remove it from valid locations so we don't duplicate.
-      validBombLocations.splice(pos, 1);
+  // Add bombs!
+  const validBombLocations = [...Array(size).keys()];
+  for (var i = 0; i < bombs; i++) {
+    // Get a random location.
+    var pos = Math.round(Math.random() * (validBombLocations.length - 0) + 0);
+    // Make it a bomb.
+    field[pos] = 9
+    // Remove it from valid locations so we don't duplicate.
+    validBombLocations.splice(pos, 1);
+  }
+
+  // Add numbers!
+  for (var n = 0; n < size; n++) {
+    let fVal = field[n];
+    if (fVal == 9) continue; // Ignore if bomb
+    var finalNumber = 0;
+
+    const numbers = [
+      -1,
+      1,
+      -(width - 1),
+      -width,
+      -(width + 1),
+      width - 1,
+      width,
+      width + 1,
+    ];
+
+    for (const x in numbers) {
+      let num = numbers[x];
+      let i = n + num;
+
+      if (i < 0 || i > size) continue; // Exit if irrelevant
+      if (
+        (n + 1) % width == 0 &&
+        (num == 1 || num == width + 1 || num == -(width - 1))
+      )
+        continue;
+      if (
+        n % width == 0 &&
+        (num == -1 || num == -(width + 1) || num == width - 1)
+      )
+        continue;
+      if (field[i] == 9) finalNumber++;
     }
 
-    // Return rows!
-    const rows = Array(height).fill('-1').map((_v, rowIndex) => {
-      return Row(
-        Array(width).fill('-1').map((_v, columnIndex) => {
-          const dataId = rowIndex * width + columnIndex;
-          return Button({
-            dataId: `${dataId}`,
-            bomb: field[dataId] === 9
-          });
-        }),
-        `${i}`
-      )
-    })
+    field[n] = finalNumber;
+  }
 
-    const grid = document.createElement('div')
-    grid.className = "grid"
-    rows.map((v) => grid.appendChild(v));
+  // Return rows!
+  const rows = Array(height).fill('-1').map((_v, rowIndex) => {
+    return Row(
+      Array(width).fill('-1').map((_v, columnIndex) => {
+        const dataId = rowIndex * width + columnIndex;
+        return Button({
+          dataId: `${dataId}`,
+          bomb: field[dataId] === 9,
+          surrounds: field[dataId]
+        });
+      }),
+      `${i}`
+    )
+  })
 
-    return grid
+  const grid = document.createElement('div')
+  grid.className = "grid"
+  rows.map((v) => grid.appendChild(v));
+
+  return grid
 }
